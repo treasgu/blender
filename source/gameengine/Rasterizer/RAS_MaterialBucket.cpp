2e0d36a8d930ac5b2bc03bead14d5dcfae444618
Porteries Tristan <republicthunderbolt9@gmail.com>
2017-06-03 18:21:39 +0200

    UPBGE: Move zoffset settings in material render node.

--- source/gameengine/Rasterizer/RAS_MaterialBucket.cpp
+++ source/gameengine/Rasterizer/RAS_MaterialBucket.cpp
@@ -57,6 +57,7 @@ RAS_MaterialBucket::RAS_MaterialBucket(RAS_IPolyMaterial *mat)
 	m_nodeData.m_zsort = m_material->IsZSort();
 	m_nodeData.m_text = m_material->IsText();
 	m_nodeData.m_useLighting = m_material->UsesLighting();
+	m_nodeData.m_zoffset = m_material->GetZOffset();
 }
 
 RAS_MaterialBucket::~RAS_MaterialBucket()
@@ -210,7 +211,10 @@ void RAS_MaterialBucket::GenerateTree(RAS_ManagerDownwardNode& downwardRoot, RAS
 void RAS_MaterialBucket::BindNode(const RAS_MaterialNodeTuple& tuple)
 {
 	RAS_ManagerNodeData *managerData = tuple.m_managerData;
-	managerData->m_rasty->SetCullFace(m_nodeData.m_cullFace);
+	RAS_Rasterizer *rasty = managerData->m_rasty;
+	rasty->SetCullFace(m_nodeData.m_cullFace);
+	rasty->SetPolygonOffset(-m_nodeData.m_zoffset, 0.0f);
+
 	if (!managerData->m_shaderOverride) {
 		ActivateMaterial(managerData->m_rasty);
 	}
